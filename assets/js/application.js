eb3a9e22e2b658c10b65b14abcd65b0a9587ac32
Bruno Henrique Stein <brunohstein@gmail.com>
2013-03-27 23:24:30 -0300

    um pouco de cor por fim das contas

--- assets/js/application.js
+++ assets/js/application.js
@@ -1,56 +1,129 @@
 var app = {
   config: {
     genre: '',
-    api_key: 'b23b7cd56841618914283936bf8e1727',
-    base_url: 'http://api.themoviedb.org/3/',
-    include_adult: true
+    baseUrl: 'http://api.filmaster.com',
+    minimumAverageScore: 7.5,
+    minimumNumberOfVotes: 500
+  },
+
+  database: {
+    movies: new Array()
   },
 
   ui: {
-    genreSelector: $('.genre-selector', '.user-input'),
-    submitBtn: $('.submit-button', '.user-input'),
-    movieList: $('.movie-list', '.search-results')
+    movieList: $('.movie-list'),
+    seenList: $('.seen-list'),
+    loadingMessage: $('.loading-message'),
+    genreSelector: $('.genre-selector')
   },
 
   init: function() {
-    app.populateGenreSelector();
-    app.binder();
+    app.ui.loadingMessage.hide();
+    app.bindGenreSelector();
   },
 
-  populateGenreSelector: function() {
-    $.getJSON(app.config.base_url + 'genre/list?api_key=' + app.config.api_key + '&callback=?', {},
-      function(data) {
-        $.each(data.genres, function(i, genre) {
-          app.ui.genreSelector.append('<option value="' + genre.id + '">' + genre.name + '</option>');
-        });
-      }
-    );
-  },
+  bindGenreSelector: function() {
+    app.ui.genreSelector.chosen({'disable_search': true});
 
-  binder: function() {
-    app.ui.submitBtn.click(function() {
-      app.getSelectedGenre();
-      app.showMovies(1);
+    app.ui.genreSelector.change(function() {
+      app.config.genre = $(this).val();
+      app.ui.genreSelector.fadeOut();
+      $(".chzn-container").fadeOut(function() {
+        $(this).remove();
+        app.ui.loadingMessage.fadeIn();
+      });
+      app.getMovies(1);
     });
   },
 
-  getSelectedGenre: function() {
-    app.config.genre = app.ui.genreSelector.val();
+  bindSeenButton: function() {
+    $('.seen-button').click(function() {
+      $(this).parents('li').fadeOut(function() {
+        $(this).remove();
+      });
+      return false;
+    });
   },
 
-  showMovies: function(page) {
-    $.getJSON(app.config.base_url + 'genre/' + app.config.genre + '/movies?include_adult= ' + app.config.include_adult + '&page=' + page + '&api_key=' + app.config.api_key + '&callback=?', {},
-      function(data) {
-        $.each(data.results, function(i, movie) {
-          app.ui.movieList.append('<li>' + movie.original_title + '</li>');
-        });
-        if (page <= data.total_pages) {
-          page++;
-          app.showMovies(page);
+  bindPassButton: function() {
+    $('.pass-button').click(function() {
+      $(this).parents('li').fadeOut(function() {
+        $(this).remove();
+      });
+      return false;
+    });
+  },
+
+  bindRetryButton: function() {
+    $('.retry-button').click(function() {
+      window.location.reload(true);
+      return false;
+    });
+  },
+
+  getMovies: function(page) {
+    $.ajax({
+      url: app.config.baseUrl + '/1.1/ranking/film/?tags= ' + app.config.genre + '&page=' + page,
+      dataType: 'jsonp',
+      success: successCallback
+    });
+
+    function successCallback(data) {
+      $.each(data.objects, function(i, movie) {
+        if (i < 9) {
+          if (movie.average_score >= app.config.minimumAverageScore & movie.number_of_votes >= app.config.minimumNumberOfVotes) {
+            app.database.movies.push(movie);
+          }
+        } else if (i == 9) {
+          if (movie.average_score >= app.config.minimumAverageScore) {
+            if (movie.number_of_votes >= app.config.minimumNumberOfVotes) {
+              app.database.movies.push(movie);  
+            }
+            page++;
+            app.getMovies(page);
+          } else {
+            app.ui.loadingMessage.fadeOut('fast');
+            app.showMovies();
+          }
         }
-      }
-    );
+      });
+    };
+  },
+
+  showMovies: function() {
+    $.each(app.database.movies, function(i, movie) {
+      app.ui.movieList.append([
+        '<li>',
+          '<div class="movie-picture">',
+            '<img src="' + app.config.baseUrl + movie.hires_image + '">',
+          '</div>',
+          '<div class="movie-id">',
+            '<h2 class="movie-title">' + movie.title_localized + '</h2>',
+            '<h3 class="movie-director">' + movie.directors[0].surname + ' (' + movie.release_year + ')</h3>',
+          '</div>',
+          '<div class="movie-info">',
+            '<p class="movie-description">' + movie.description + '</p>',
+            '<a href="#" class="seen-button">I have already seen it!</a>',
+            '<a href="#" class="pass-button">Hm, not now.</a>',
+          '</div>',
+        '</li>'
+      ].join(''));
+    });
+    app.ui.movieList.append([
+      '<li>',
+        '<h3 class="the-end">The end.',
+          '<small>These are not the droid you are looking for.</small>',
+        '</h3>',
+        '<a href="#" class="retry-button">Try another genre</a>',
+      '</li>'
+      ].join(''));
+    app.bindSeenButton();
+    app.bindPassButton();
+    app.bindRetryButton();
   }
 };
 
-$(document).ready(function() { app.init(); });
+$(document).ready(function() {
+  app.init();
+  $('body').addClass('visible');
+});
